1c18e022e7e9ca6a298451ed60a5803b9ded1ee7
Jakub Podgrodzki <podgrodzkijakub@gmail.com>
2025-02-10 14:54:46 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+function getEmployeeData() {
+  const employees = [];
+  const listItems = document.querySelectorAll('ul li');
+
+  listItems.forEach((item) => {
+    const employeeName = item.textContent.trim();
+    const position = item.getAttribute('data-position');
+    const salary = parseInt(
+      item.getAttribute('data-salary').replace(/\$|,/g, ''),
+      10,
+    );
+    const age = item.getAttribute('data-age');
+
+    employees.push({
+      employeeName,
+      position,
+      salary,
+      age,
+    });
+  });
+
+  return employees;
+}
+
+function sortEmployees() {
+  const employees = getEmployeeData();
+
+  return employees.sort((a, b) => b.salary - a.salary);
+}
+
+sortEmployees();
